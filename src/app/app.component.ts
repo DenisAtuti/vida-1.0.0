bded1fbf97e208fd73ea95ed7d51e54f1f69b4b4
Dennis Atuti <atutidennis@gmail.com>
2023-01-22 10:53:07 +0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { IsLoggedService } from './Services/is-logged.service';
 import { LandingService } from './Services/landing.service';
@@ -9,16 +9,20 @@ import { SetScrolledHeightService } from './Services/set-scrolled-height.service
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent implements OnInit,  AfterViewInit, OnDestroy{
   constructor(
     private landingService: LandingService, 
+    private renderer: Renderer2, 
     private logged: IsLoggedService,
     private scroll: SetScrolledHeightService
     ){}
   
+  
   title = 'vida';
   isLanding: boolean = false;
   landingSub: Subscription
+  @ViewChild("content", {static: false}) contentEl: ElementRef;
+  content:any;
 
   ngOnInit(): void {
     this.landingSub = this.landingService.landingSubj.subscribe(
@@ -29,6 +33,14 @@ export class AppComponent implements OnInit, OnDestroy{
     this.isLanding = localStorage.getItem("isLanding") === 'true' ? true : false;
     this.logged.verifyToken()
     this.scroll.setAllComponentScollToDefault();
+    
+  }
+
+  ngAfterViewInit(): void {
+    this.content = this.contentEl.nativeElement
+    this.renderer.listen(this.content, 'mouseenter' ,()=>{
+      this.fullScreenOnMobile()
+    })
   }
 
   openLanding(){
@@ -37,6 +49,13 @@ export class AppComponent implements OnInit, OnDestroy{
       this.landingService.landingSubj.next(true)
       console.log("subjecting");
     }, 10000);
+  }
+
+  fullScreenOnMobile(){
+    if (document.fullscreenElement == null && window.innerWidth <= 768) {
+      this.content.requestFullscreen()
+      console.log(this.content)
+    }
   }
 
 
