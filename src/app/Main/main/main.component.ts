b686738d1ecb8198101c948cf8e555a318adf21b
Dennis Atuti <atutidennis@gmail.com>
2023-02-25 15:39:23 +0300

    fg

--- src/app/Main/main/main.component.ts
+++ src/app/Main/main/main.component.ts
@@ -56,11 +56,12 @@ export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
         this.posts = response;
 
 
-        this.title.setTitle('Vida videos the new age Porn viewing and watchig');
+        this.title.setTitle('Vida videos the new age Porn viewing and watching');
         this.meta.updateTag({
           name: 'description',
           content: `vida videos the world's best porn sites of ${this.state.getYeah()}. Watch free porn videos, 
-          sex movies and premium HD porn on the most popular porn tubes. All the top porn ...`,
+          sex movies and premium HD porn on the most popular porn tubes. All the top porn like Hentai ,Well hung,Huge breasts,
+          Anal, Ebony,Mature, Teen,Amateur,MILF,Lesbian etc`,
         });
       });
 
