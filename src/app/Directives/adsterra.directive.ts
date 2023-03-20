614cdd791b861a6b2a4f70fa6d50b595cf89a746
Dennis Atuti <atutidennis@gmail.com>
2023-03-20 12:09:11 +0300

    gf

--- src/app/Directives/adsterra.directive.ts
+++ src/app/Directives/adsterra.directive.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Directive, ElementRef, Inject } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Inject, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appAdsterra]'
@@ -10,11 +10,13 @@ export class AdsterraDirective implements AfterViewInit  {
 
   constructor(
     private el: ElementRef,
+    private renderer: Renderer2,
     @Inject(DOCUMENT) private document: Document               
   ) { }
 
   ngAfterViewInit(){
     const templateEl = this.el.nativeElement as HTMLElement;
+    this.renderer.addClass(this.el.nativeElement, "adsterra")
     const scriptEl = this.document.createElement('script');
     const adEl = this.document.createElement('div');
 
@@ -41,16 +43,29 @@ export class AdsterraDirective implements AfterViewInit  {
 
   createNativeAd(){
     const nativeAdEl = document.createElement("a")
-    // nativeAdEl.setAttribute("href","https://www.highrevenuegate.com/qv905grx?key=be9d6988c6876b6c623e1c8612170918");
     nativeAdEl.setAttribute("href","https://t.adtng2.com/228803/3785/0?bo=2753,2754,2755,2756&pyt=multi&po=6456");
     nativeAdEl.setAttribute("target","_blank");
-
+    nativeAdEl.style.inset = '0';
+    nativeAdEl.style.display = 'flex';
+    nativeAdEl.style.justifyContent = 'center';
+    nativeAdEl.style.alignItems = 'center';  
+    
     const nativeAdImage = document.createElement("img")
-    nativeAdImage.setAttribute("src","https://vida-videos.com/uploads/images/vida-3.gif")
-    nativeAdEl.appendChild(nativeAdImage)
+    nativeAdImage.setAttribute("src",`https://vida-videos.com/uploads/images/vida-${this.getRandomNumber()}.gif`)
+    nativeAdImage.alt = "This is an add"
+    nativeAdImage.style.height = '100%';
+    nativeAdImage.style.width = 'auto';
+    nativeAdImage.style.objectFit = 'contain';
+
+
+    nativeAdEl.appendChild(nativeAdImage); 
 
     return nativeAdEl;
 
+  }
+
+  getRandomNumber(){
+    return Math.floor(Math.random() * 20) + 1;
   }
 
 
